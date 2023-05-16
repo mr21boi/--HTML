ec92d13154505a1b9b661ea4b18a538a959e054c
collins <mr21boi@gmail.com>
2023-05-15 19:13:31 -0700

    all commit

---
+++ FirstJS.js
@@ -0,0 +1,43 @@
+let arr = ['rock', 'paper' , 'scissors'];
+
+function getPCHand(){
+    return parseInt ((math.random()*10)%3);
+}
+
+function compare (choice1, choice2){
+    let firstperson = getPCHand;
+    let secondperson = getPCHand;
+
+    if (firstperson > secondperson){
+        console.log('great job! ${choice1}, you won with ${firstperson}');
+    } else {
+        console.log('great job! ${choice1}, you won with ${firstperson}'
+    }
+}
+
+function outer () {
+    let x = '1';
+    (function inner () {
+        let y = '2';
+        console.log(x);)
+    }
+}
+
+outer();
+
+
+//THIS//
+
+
+let teacher = {
+    name: "collins"
+    
+}
+
+console.log {
+CHAnGES MADEe
+}
+/// REVIEW 
+
+
+d
